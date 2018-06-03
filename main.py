28a34f33ee0e705fe3ea52dd7170507cb52d6e06
Loic Alain Royer <royerloic@gmail.com>
2018-06-02 22:15:16 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+import numpy as np
+from napari.gui.image_display import ImageDisplay
+
+if __name__ == '__main__':
+
+    # create an image
+    h = 512
+    w = 512
+    Y, X = np.ogrid[-2.5:2.5:h * 1j, -2.5:2.5:w * 1j]
+    image = np.empty((h, w), dtype=np.float32)
+    image[:] = np.exp(- X ** 2 - Y ** 2)  # * (1. + .5*(np.random.rand(h, w)-.5))
+    image[-30:] = np.linspace(0, 1, w)
+
+    # starting
+    app = QApplication(sys.argv)
+
+    imgdis = ImageDisplay(image)
+    imgdis.show()
+
+    sys.exit(app.exec_())
